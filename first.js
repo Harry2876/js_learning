95ef62e2f60b6c6e222c0bc5bff6890f2741eb74
ʜᴀʀɪᴏᴍ ʜᴀʀꜱʜ <132590839+Harry2876@users.noreply.github.com>
2024-02-15 01:40:58 +0530

    Initial Commit

---
+++ first.js
@@ -0,0 +1,18 @@
+const student = {
+    fullName: "Hariom Harsh",
+    age:20,
+    cgpa:9.1,
+    isPass:true,
+};
+
+console.log(student);
+
+//Arithmetic Operators
+let a=5;
+let b=2;
+
+console.log("a=" , a , " b =" , b);
+console.log("a + b =", a + b);
+console.log("a - b" , a-b);
+console.log("a * b", a*b);
+console.log("a / b", a/b);
