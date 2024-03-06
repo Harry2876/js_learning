f7b2308ec3a5c7ffdfe7a959128f74f71b56534d
ʜᴀʀɪᴏᴍ ʜᴀʀꜱʜ <132590839+Harry2876@users.noreply.github.com>
2024-03-07 02:03:47 +0530

    functions in js

---
+++ script.js
@@ -0,0 +1,13 @@
+//Functions in js
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges}
+     oranges.`;
+     return juice;
+}
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2,7);
+
+console.log(appleOrangeJuice);
